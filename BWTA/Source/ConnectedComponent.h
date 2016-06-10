7e680cb33185a9b383dcc457d2ae78444db133d5
Alberto Uriarte <albertouri@drexel.edu>
2016-06-10 17:09:35 -0400

    Refactored ConnectedComponent

--- BWTA/Source/ConnectedComponent.h
+++ BWTA/Source/ConnectedComponent.h
@@ -1,19 +1,29 @@
 #pragma once
+
 #include "functions.h"
+
 namespace BWTA
 {
-  class ConnectedComponent
-  {
-  public:
-    ConnectedComponent(int id, bool walkable);
-    bool isWalkable(void) const;
-    void setWalkability(bool walkability);
-    int getID(void) const;
-    Point top_left() const;
-    void set_top_left(Point top_left_tile);
-    private:
-    Point top_left_tile;
-    bool _walkability;
-    int _id;
-  };
+	class ConnectedComponent
+	{
+	public:
+		ConnectedComponent(int id, bool walkable = true)
+			: _id(id)
+			, _walkability(walkable)
+			, _topLeftTile(10000, 10000)
+		{};
+
+		const int getID() const { return _id; };
+
+		const bool isWalkable() const { return _walkability; };
+		void setWalkability(bool walkability) { _walkability = walkability; };
+		
+		const Point top_left() const { return _topLeftTile; };
+		void set_top_left(Point topLeftTile) { _topLeftTile = topLeftTile; };
+
+	private:
+		int _id;
+		bool _walkability;
+		Point _topLeftTile;
+	};
 }
